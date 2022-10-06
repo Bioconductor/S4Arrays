e14f4b27036d7180780ebb2d7891928a77e555f0
Hervé Pagès <hpages.on.github@gmail.com>
2022-10-06 12:38:21 -0700

    Get rid of the ArraySelection class hierarchy (not used)

--- src/R_init_S4Arrays.c
+++ src/R_init_S4Arrays.c
@@ -1,7 +1,6 @@
 #include <R_ext/Rdynload.h>
 
 #include "abind.h"
-#include "ArraySelection_class.h"
 #include "array_selection.h"
 
 #define CALLMETHOD_DEF(fun, numArgs) {#fun, (DL_FUNC) &fun, numArgs}
@@ -10,11 +9,6 @@ static const R_CallMethodDef callMethods[] = {
 
 /* abind.c */
 	CALLMETHOD_DEF(C_abind, 3),
-
-/* ArraySelection_class.c */
-	CALLMETHOD_DEF(C_get_SelectionTree_length, 2),
-	CALLMETHOD_DEF(C_from_SelectionTree_to_matrix, 2),
-	CALLMETHOD_DEF(C_from_matrix_to_SelectionTree, 2),
 
 /* array_selection.c */
 	CALLMETHOD_DEF(C_Lindex2Mindex, 3),
