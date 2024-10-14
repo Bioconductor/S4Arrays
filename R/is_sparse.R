abdba2838565283c182c5e3c07c214f969900295
Hervé Pagès <hpages.on.github@gmail.com>
2024-10-14 10:34:52 -0700

    S4Arrays 1.5.11
    Replace is_sparse() methods for dg[C|R]Matrix and lg[C|R]Matrix objects
    with methods for [C|R|T]sparseMatrix derivatives.

--- R/is_sparse.R
+++ R/is_sparse.R
@@ -5,7 +5,7 @@
 
 ### IMPORTANT: Array-like objects for which is_sparse() is TRUE **must**
 ### support SparseArray::extract_sparse_array(). More about this in the
-### SparseArray-subsetting.R file from the SparseArray package where the
+### extract_sparse_array.R file from the SparseArray package where the
 ### extract_sparse_array() generic is defined.
 
 setGeneric("is_sparse", function(x) standardGeneric("is_sparse"))
@@ -13,13 +13,13 @@ setGeneric("is_sparse", function(x) standardGeneric("is_sparse"))
 ### By default, nothing is considered sparse.
 setMethod("is_sparse", "ANY", function(x) FALSE)
 
-### The corresponding (and **mandatory**, see IMPORTANT note above)
-### SparseArray::extract_sparse_array() methods are defined in the
-### SparseArray package.
-setMethod("is_sparse", "dgCMatrix", function(x) TRUE)
-setMethod("is_sparse", "lgCMatrix", function(x) TRUE)
-setMethod("is_sparse", "dgRMatrix", function(x) TRUE)
-setMethod("is_sparse", "lgRMatrix", function(x) TRUE)
+### All [C|R|T]sparseMatrix derivatives are considered sparse. Note that
+### they all support SparseArray::extract_sparse_array() via the default
+### extract_sparse_array() method defined in the SparseArray package.
+setMethod("is_sparse", "CsparseMatrix", function(x) TRUE)
+setMethod("is_sparse", "RsparseMatrix", function(x) TRUE)
+setMethod("is_sparse", "TsparseMatrix", function(x) TRUE)
+
 
 setGeneric("is_sparse<-", signature="x",
     function(x, value) standardGeneric("is_sparse<-")
